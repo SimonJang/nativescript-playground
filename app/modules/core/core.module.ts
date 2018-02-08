d8c4a9ccd088fe8671c9d38fa191cc691502260b
Simon <simon.jang.contact@gmail.com>
2018-02-08 20:21:56 +0100

    initial

---
+++ app/modules/core/core.module.ts
@@ -0,0 +1,6 @@
+import {NgModule} from '@angular/core';
+import {DatabaseService} from './services/database.service'
+import {LogService} from './services/log.service'
+
+@NgModule({})
+export class CoreModule { }
